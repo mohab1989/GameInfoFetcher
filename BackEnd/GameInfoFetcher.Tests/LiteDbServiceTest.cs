9acd7afc390b6b51c2e35ee4ee665e17901f058a
Mohab Emad Mohammed Meshref <mohab1989@hotmail.com>
2025-02-13 13:45:47 +0100

    Fix LiteDB update

---
+++ BackEnd/GameInfoFetcher.Tests/LiteDbServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using GameInfoFetcher.Services;
+using GameInfoFetcher.Services.Interfaces;
+
+namespace GameInfoFetcher.Tests;
+
+[TestClass]
+public
+class LiteDbServiceTest
+{
+    IServerlessDbService _db;
+    string _slug = "slug-test", _id = "12345", _dbPath = ".\\GameInfoFetcherTest.db";
+
+    [TestInitialize]
+    public void Setup()
+    {
+        Cleanup();
+        _db = new LiteDbService($"Filename={_dbPath}");
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        if (_db is not null) _db.Dispose();
+        if (File.Exists(_dbPath)) File.Delete(_dbPath);
+    }
+
+
+    [TestMethod]
+    public async
+    Task TestUpdate()
+    {
+        Assert.IsTrue(await _db.SetSlugIdMapping(_slug, _id)); // insert
+        Assert.IsTrue(await _db.SetSlugIdMapping(_slug, _id)); // update same value
+        Assert.IsTrue(await _db.SetSlugIdMapping(_slug, "12346")); // update different value
+    }
+
+    [TestMethod]
+    public async
+    Task TestGetIdBySlug()
+    {
+        Assert.IsTrue(await _db.SetSlugIdMapping(_slug, _id));
+        Assert.AreEqual(_id, await _db.GetIdBySlug(_slug));
+    }
+}
